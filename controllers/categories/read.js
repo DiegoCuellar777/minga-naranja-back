106752413acba6da5760cf26df3d32014ea48a6f
DiegoCuellar777 <diegocuellar7@gmail.com>
2023-04-24 23:30:41 -0500

    Async function para categories, retorna las categorias si exitoso

--- controllers/categories/read.js
+++ controllers/categories/read.js
@@ -1,7 +1,19 @@
-let read = (req, res, next)=>res.status(200).render('index', { 
-    title: 'CATEGORIES',
-    subtitle: "Endpoints of categories"
-})
+import Category from "../../models/Category.js"
 
+let read = async(req, res, next)=>{     // La funcion controladora debe ser asincrona para poder esperar la respuesta de Mongo
+    try {                               // Utilizo la sintaxis de try/catch para intentar algo y catchear los errores que puedan surgir 
+        let all = await Category.find() //Utilizo el metodo find() para buscar todos los recursos del modelo (que en este caso es CATEGORY) 
+        return res.status(200)          //Configuro la respuesta que le tengo que enviar al cliente (front)
+                .json({
+                    categories:all
+                })
+    } catch(error){
+        console.log(error)
+        return res.status(400)
+                .json({
+                    error: "Ha ocurrido un error"
+                })
+    }
+}
 
 export default read 
