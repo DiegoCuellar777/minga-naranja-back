64520e728caa66366c8f70dbe83a75548ba86214
DiegoCuellar777 <diegocuellar7@gmail.com>
2023-04-24 23:45:34 -0500

    Cors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import express from 'express';
 import path from "path";
 import cookieParser from 'cookie-parser';
 import logger  from'morgan';
+import cors from "cors";
 
 import indexRouter from'./routes/index.js';
 
@@ -22,6 +23,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
