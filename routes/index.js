aa646d7427e91d7736c7517324a1df3d218bc590
DiegoCuellar777 <diegocuellar7@gmail.com>
2023-04-24 23:06:55 -0500

    B-08 Endpoints read added

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
 
 import express from 'express';
+import userRouter from "./users.js"
+import authorRouter from "./authors.js"
+import categoryRouter from "./categories.js"
+import companyRouter from "./companies.js"
+import chapterRouter from "./chapters.js"
+import mangaRouter from "./mangas.js"
+
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'MINGA API',
+    subtitle: "Endpoints of Minga"
 });
+});
+
+router.use("/auth", userRouter)
+router.use("/authors", authorRouter)
+router.use("/categories", categoryRouter)
+router.use("/companies", companyRouter)
+router.use("/mangas", mangaRouter)
+router.use("/chapters", chapterRouter)
 
 export default router
