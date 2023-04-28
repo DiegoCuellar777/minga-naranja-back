aa43dfaf006d1f3199e1fe9ef95333765a969117
DiegoCuellar777 <diegocuellar7@gmail.com>
2023-04-28 10:35:12 -0500

    Template Back Sprint 2

--- app.js
+++ app.js
@@ -3,26 +3,36 @@
 import 'dotenv/config.js'
 import "./config/database.js"
 
-import createError  from "http-errors";
-import express from 'express';
+import createError  from "http-errors"
+import express from 'express'
 
-import path from "path";
-import cookieParser from 'cookie-parser';
-import logger  from'morgan';
-import cors from "cors";
+import path from "path"
+import cookieParser from 'cookie-parser'
+import logger  from'morgan'
+import cors from "cors"
 
-import indexRouter from'./routes/index.js';
+import indexRouter from'./routes/index.js'
+import notFound from './middlewares/notFound.js'
+import errorHandler from './middlewares/errorHandler.js'
+
+
 
 
 import {__dirname} from './utils.js'
 
 
-const app = express();
+
+const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use((req, res, next)=>{
+  console.log("logged")
+
+  next()
+})
 
 app.use(cors())
 app.use(logger('dev'));
@@ -31,7 +41,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', indexRouter)
+
+app.use(notFound)
+app.use(errorHandler)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/categories/read.js
+++ controllers/categories/read.js
@@ -1,18 +1,24 @@
 import Category from "../../models/Category.js"
+import createHttpError from "http-errors"
 
 let read = async(req, res, next)=>{     // La funcion controladora debe ser asincrona para poder esperar la respuesta de Mongo
     try {                               // Utilizo la sintaxis de try/catch para intentar algo y catchear los errores que puedan surgir 
         let all = await Category.find() //Utilizo el metodo find() para buscar todos los recursos del modelo (que en este caso es CATEGORY) 
-        return res.status(200)          //Configuro la respuesta que le tengo que enviar al cliente (front)
-                .json({
-                    categories:all
-                })
+        
+
+
+        if(all.length>0){
+            return res.status(200)          
+            .json({
+                categories: all
+            })
+        }
+        
+        return next(createHttpError(404, "El recurso no se encontró"))
+        
     } catch(error){
         console.log(error)
-        return res.status(400)
-                .json({
-                    error: "Ha ocurrido un error"
-                })
+        next(error)
     }
 }
 
