feb12dbccfb1c0b9145a1640d3426e4785780b29
Carlos Arturo Torres <carlosat95@gmail.com>
2023-05-03 16:20:30 -0500

    Passport

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,9 +1,6 @@
 const validator = (schema) => [
     (req, res, next) => {
-        console.log('Este es req body',req.body);
-        console.log('Esta es el shcema',schema._ids._byKey);
         const validation = schema.validate(req.body, { abortEarly: false })
-
         if (validation.error) {
             return res.status(400).json({
                 success: false,
--- routes/chapters.js
+++ routes/chapters.js
@@ -2,12 +2,14 @@ import { Router } from "express"
 import read from "../controllers/chapters/read.js"
 import create from "../controllers/chapters/create.js"
 import {createChapter} from '../schemas/chapters.js'
+import authorizedRole from "../middlewares/authorizedRole.js"
 import validator from "../middlewares/validator.js"
+import passport from "passport"
 
 let router = Router()
 
 router.get("/", read)
 
-router.post("/chapter-form",validator(createChapter),create)
+router.post("/chapter-form",passport.authenticate('jwt', {session:false}),authorizedRole,validator(createChapter),create)
 
 export default router
