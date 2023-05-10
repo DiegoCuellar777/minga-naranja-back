ee19964caf901817cbc4b47b50484f88ac7c0517
Nicolas <nilonb7@gmail.com>
2023-05-10 15:58:11 -0300

    middlewares controlador y routes mangas

---
+++ controllers/mangas/get_mangas.js
@@ -0,0 +1,48 @@
+import MangaModel from '../../models/Manga.js' 
+
+let get_mangas = async(req, res, next)=> {
+    //queries.author_id = queries.author_id ?? ''
+    //queries.order = queries.order ?? 1 
+    //queries.category_id = queries.category_id ?? ''
+    let queries = {}
+    let sort = {}
+    let pagination = {
+        limit: 6,
+        page: 1
+    } 
+    if(req.query.title) {
+        queries.title = new RegExp(req.query.title.trim(),'i')
+    }
+    if(req.query.category_id) {
+        queries.category_id = req.query.category_id.split(',')
+    }
+    if(req.query.author_id) {
+        queries.author_id = req.query.author_id
+    }
+    if(req.query.order) {
+        sort.title = req.query.order
+    } 
+    if(req.query.limit) {
+        pagination.limit = req.query.limit
+    }
+    if(req.query.page) {
+        pagination.page = req.query.page
+    }
+    //por default 1 ordena de menor a mayor
+    //por default -1 ordena de mayor a menor
+    try {
+        let all = await MangaModel
+        .find(queries)                                                          //primero lo encuentro
+        .sort(sort)                                                             //segundo lo ordeno
+        .skip( pagination.page > 0 ? (pagination.page-1)*pagination.limit : 0 ) //tercero lo pagino
+        .limit( pagination.limit > 0 ? pagination.limit : 0 )
+        return res.status(200).json({
+            succes: true,
+            response: all
+        })
+    } catch (error) {
+        next (error)
+    }
+}
+
+export default get_mangas
--- routes/mangas.js
+++ routes/mangas.js
@@ -1,17 +1,18 @@
 // Se definen los endpoints de los mangas
 // y se exportan para poder utilizarlos en el enrutador PRINCIPAL
 import { Router } from "express"
-import read from "../controllers/mangas/read.js"
+import read from "../controllers/mangas/get_mangas.js"
 import create from "../controllers/mangas/create.js"
 import passport from "passport"
 
 import { Mangas } from "../schemas/mangas.js"
 import validator from "../middlewares/validator.js"
+import exists_title from '../middlewares/exists_title.js'
 
 let router = Router()
 
 router.get("/", passport.authenticate('jwt', {session:false}), read)
-router.post('/', validator(Mangas), create)
+router.post('/', validator(Mangas), exists_title, create)
 
 
 router.get("/mangas",(req, res, next)=>res.status(201).json({
