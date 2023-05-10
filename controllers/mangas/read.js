ff3030d5dbe99366b33ddd13e9fff5557c7bc040
Nico Barrera <57334167+nicodsd@users.noreply.github.com>
2023-05-10 16:00:52 -0300

    M 03 (#28)
    * cambios en read, y routes
    
    * middlewares controlador y routes mangas

--- controllers/mangas/read.js
+++
@@ -1,7 +0,0 @@
-let name = "mangas"
-let read = (req, res, next)=>res.status(200).render('index', { 
-    title: '/'+ name.toUpperCase() ,
-    subtitle: "Endpoints of "+ name
-})
-
-export default read
--- routes/mangas.js
+++ routes/mangas.js
@@ -1,24 +1,21 @@
 // Se definen los endpoints de los mangas
 // y se exportan para poder utilizarlos en el enrutador PRINCIPAL
 import { Router } from "express"
-import read from "../controllers/mangas/read.js"
+import read from "../controllers/mangas/get_mangas.js"
 import create from "../controllers/mangas/create.js"
 import getMangasFromAuthor from "../controllers/mangas/get_mangas_from_autor.js"
 import passport from "passport"
 
-import { MangaForm } from "../schemas/mangas.js"
+import { Mangas } from "../schemas/mangas.js"
 import validator from "../middlewares/validator.js"
+import exists_title from '../middlewares/exists_title.js'
 
 let router = Router()
 
 router.get("/", passport.authenticate('jwt', {session:false}), read)
-router.post('/', validator(MangaForm), create)
+router.post('/', validator(Mangas), exists_title, create)
 
 router.get("/:author_id", getMangasFromAuthor);
-
-router.get('/', function(req,res,next) {
-    res.send('respond with a resource')
-})
 
 router.get("/mangas",(req, res, next)=>res.status(201).json({
     succes:true,
