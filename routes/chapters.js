6f9ec00cbe1f26c32e016b23c8df174f9733e129
CATorres Varon <44732360+ctorresv@users.noreply.github.com>
2023-05-11 10:04:04 -0500

    M 08 02 (#35)
    * Correcciones con get manga
    
    * Fixes m-08-02
    
    * Fixes m-08-02

--- routes/chapters.js
+++ routes/chapters.js
@@ -10,10 +10,9 @@ import get_chapters from "../controllers/chapters/get_chapters.js"
 
 let router = Router()
 
-router.get("/:_id", one)
 router.get("/", read)
 router.get("/get", get_chapters)
-
+router.get("/:_id", one)
 
 router.post("/chapter-form",passport.authenticate("jwt",{session:false}),validator(createChapter),authorizedRole,create)
 
