592111ae6c697d6489e1bdc51cb0eebe4a631997
DiegoCuellar777 <112589821+DiegoCuellar777@users.noreply.github.com>
2023-05-18 20:46:34 -0500

    Primera version back (#41)

--- middlewares/finds_id.js
+++ middlewares/finds_id.js
@@ -1,26 +1,34 @@
 import Author from "../models/Author.js";
-import Company from "../models/Company.js"
+import Company from "../models/Company.js";
 
-async function finds_id(req, res, next) {
-    const user_id = req.user.id
+const finds_id = async (req, res, next) => {
+  const userId = req.user.id; // Obtener el ID del usuario autenticado
 
     try {
-        const author = await Author.findOne({ user_id })
-        if (author) {
-            req.body.author_id = author._id;
-            return next()
-        }
+    // Buscar el autor correspondiente según el ID del usuario logueado
+    const author = await Author.findOne({ user_id: userId });
 
-        const company = await Company.findOne({ user_id })
-        if (company) {
-            req.body.company_id = company._id;
-            return next()
-        }
-
-        return res.status(404).json({ message: 'Author or company not found' })
-    } catch (error) {
-        return res.status(500).json({ message: error.message })
+    if (author) {
+      req.body.author_id = author._id; // Agregar la propiedad author_id al req.body
+      return next(); // Pasar al siguiente middleware o controlador
     }
-}
 
-export default finds_id
+    // Buscar la empresa correspondiente según el ID del usuario logueado
+    const company = await Company.findOne({ user_id: userId });
+
+    if (company) {
+      req.body.company_id = company._id; // Agregar la propiedad company_id al req.body
+      return next(); // Pasar al siguiente middleware o controlador
+    }
+
+    return res.status(404).json({
+        error: "Autor o empresa no encontrado para el usuario logueado",
+    });
+    } catch (error) {
+    return res.status(400).json({
+        error: "Ha ocurrido un error",
+    });
+    }
+};
+
+export default finds_id;
